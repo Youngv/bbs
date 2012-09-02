024203c6828ab08b502298dd56c97c50bfb16e7a
Youngv <ootyoungtoo@qq.com>
2012-09-02 11:34:39 +0800

    fix some css problem

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   attr_accessible :content, :title, :board_id
   belongs_to :user
-  validates :title, :content, presence: true
+  validates :title, :content, :board_id, presence: true
   has_many :comments
   belongs_to :board
 end
